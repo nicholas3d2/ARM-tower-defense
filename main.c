728c6637f5ff69b37272b67dc015c3cf067b42a6
nicholas3d2 <nicholas3d2@gmail.com>
2021-04-01 09:45:04 -0400

    Added header file data into main.c for testing on CPULator. JTAG UART functions working.

--- main.c
+++ main.c
@@ -1,7 +1,64 @@
-/* Description: main.c file for tower defense game
+/* Description: main.c file for tower defense game, developed for CPULATOR
  */
+//#include "main.h"
 
-#include "main.h"
+/************main.h************/
+#define SDRAM_BASE            0xC0000000
+#define FPGA_ONCHIP_BASE      0xC8000000
+#define FPGA_CHAR_BASE        0xC9000000
+
+/* Cyclone V FPGA devices */
+#define LEDR_BASE             0xFF200000
+#define HEX3_HEX0_BASE        0xFF200020
+#define HEX5_HEX4_BASE        0xFF200030
+#define SW_BASE               0xFF200040
+#define KEY_BASE              0xFF200050
+#define TIMER_BASE            0xFF202000
+#define PIXEL_BUF_CTRL_BASE   0xFF203020
+#define CHAR_BUF_CTRL_BASE    0xFF203030
+#define JTAG_UART_BASE        0xFF201000
+
+/* VGA colors */
+#define WHITE 0xFFFF
+#define YELLOW 0xFFE0
+#define RED 0xF800
+#define GREEN 0x07E0
+#define BLUE 0x001F
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define GREY 0xC618
+#define PINK 0xFC18
+#define ORANGE 0xFC00
+
+#define ABS(x) (((x) > 0) ? (x) : -(x))
+
+/* Screen size. */
+#define RESOLUTION_X 320
+#define RESOLUTION_Y 240
+
+/* Constants for animation */
+#define BOX_LEN 2
+#define NUM_BOXES 8
+
+#define FALSE 0
+#define TRUE 1
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void plot_pixel(int x, int y, short int line_color);
+void draw_line(int x0, int y0, int x1, int y1, short int line_color);
+void clear_screen();
+void wait_for_vsync();
+void draw_box(int x, int y, short int line_colour);
+
+char get_jtag(volatile int * JTAG_UART_ptr);
+void put_jtag(volatile int * JTAG_UART_ptr,char c);
+
+volatile int pixel_buffer_start; // global variable
+
+/************main.h************/
 
 int main(void) {
     volatile int * JTAG_UART_ptr = (int *)0xFF201000;// JTAG UART address
