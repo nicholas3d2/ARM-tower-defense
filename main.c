d61d6ca09c717c87232a53cf0603a84ae094585d
Jintao Huang <57041780+Galladerotom@users.noreply.github.com>
2021-04-01 23:14:38 -0400

    Changed function name draw_diamond to draw_turret_diamond

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ void draw_line(int x0, int y0, int x1, int y1, short int line_color);
 void clear_screen();
 void wait_for_vsync();
 void draw_box(int x, int y, short int line_colour);
-void draw_diamond(int x, int y, short int line_colour);
+void draw_turrent_diamon(int x, int y, short int line_colour);
 
 char get_jtag(volatile int *JTAG_UART_ptr);
 void put_jtag(volatile int *JTAG_UART_ptr, char c);
@@ -105,12 +105,12 @@ int main(void) {
   // Main program loop, read user inputs while running
   while (1) {
     // clear 2 frames before
-    draw_diamond(xprev2, yprev2, 0);
+    draw_turrent_diamon(xprev2, yprev2, 0);
     draw_grid_box(xprev2, yprev2, 0);
 
     // draw
     draw_grid_box(xcurrent, ycurrent, WHITE);
-    draw_diamond(xcurrent, ycurrent, WHITE);
+    draw_turrent_diamon(xcurrent, ycurrent, WHITE);
     // update position
     xprev2 = xprev1;
     yprev2 = yprev1;
@@ -246,7 +246,7 @@ void draw_box(int x, int y, short int line_colour) {
 }
 
 // draws the diamond for a 20x20 box
-void draw_diamond(int x, int y, short int line_colour) {
+void draw_turrent_diamond(int x, int y, short int line_colour) {
   draw_line(x + 9, y + 2, x + 2, y + 9, line_colour); // top left edges
   draw_line(x + 9, y + 3, x + 3, y + 9, line_colour);
   draw_line(x + 10, y + 2, x + 17, y + 9, line_colour); // top right edges
